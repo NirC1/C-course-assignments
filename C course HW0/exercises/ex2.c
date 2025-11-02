052fd0430ced5f9a78659693faa1a15d93cdcd14
NirC1 <140725334+NirC1@users.noreply.github.com>
2025-11-02 13:44:03 +0200

    added ex2.c without fixing

---
+++ C course HW0/exercises/ex2.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+print(" /\n")
+print(" / \/n")
+print(" / \n")
+print("/ \n")
+print(" ---- /n")
+print("| _ |n")
+print("| |_||n"
+print("| __|\n"
+print("|| | |\n")
+print("|| | |\n")
+}
