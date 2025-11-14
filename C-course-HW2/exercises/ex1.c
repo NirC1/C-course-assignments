9f4d50a4ff1656f50aeff8e9bdd17cf5e13f58b9
NirC1 <140725334+NirC1@users.noreply.github.com>
2025-11-14 15:02:29 +0200

    changed types to unsigned

--- C-course-HW2/exercises/ex1.c
+++ C-course-HW2/exercises/ex1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int count_num_even_digits(int num, int count);
+unsigned int count_num_even_digits(unsigned int num, unsigned int count);
 
 int main() {
   unsigned int num;
@@ -21,13 +21,13 @@ int main() {
   receives num and count (0) and calls itself with count +1 if the last digit is even
   calls itself with count +0 if last digit isn't even, returns if the number doesn't contain any more digits.
 */
-int count_num_even_digits(int num, int count)
+unsigned int count_num_even_digits(unsigned int num, unsigned int count)
 {
   if ( num  == 0 ){
     return count;
   }
 
-  int digit = 0;
+  unsigned int digit = 0;
   digit = num % 10;
   if ( 0 == digit || ((digit % 2) == 1) ){
     return count_num_even_digits(num / 10, count);
