3e89caa94168ee3c6ed590799469fdf12e813521
NirC1 <140725334+NirC1@users.noreply.github.com>
2025-11-14 14:46:15 +0200

    working version of hw2-ex1

--- C-course-HW2/exercises/ex1.c
+++ C-course-HW2/exercises/ex1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// TODO: add your recursive function implementation here
+int count_num_even_digits(int num, int count);
 
 int main() {
   unsigned int num;
@@ -10,9 +10,28 @@ int main() {
   printf("Please insert a non-negative integer:\n");
   scanf("%u", &num);
 
-  // TODO: Call the recursive function and display result
+  result = count_num_even_digits(num, 0);
 
   // Prompt result to user
   printf("The number of even digits in %u is: %u\n", num, result);
   return 0;
+}
+
+/*
+  receives num and count (0) and calls itself with count +1 if the last digit is even
+  calls itself with count +0 if last digit isn't even, returns if the number doesn't contain any more digits.
+*/
+int count_num_even_digits(int num, int count)
+{
+  if ( num  == 0 ){
+    return count;
+  }
+
+  int digit = 0;
+  digit = num % 10;
+  if ( 0 == digit || ((digit % 2) == 1) ){
+    return count_num_even_digits(num / 10, count);
+  }else{
+    return count_num_even_digits(num / 10, count +1);
+  }
 }
